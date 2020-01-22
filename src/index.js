28dbdeb4acf175580fb20191d4a3d69555e91de3
Sam <samuel_morgan_@outlook.com>
2020-01-22 22:45:33 +0000

    removed unique key warning

--- src/index.js
+++ src/index.js
@@ -3,17 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Square(props) {
-	return <div className="box" />;
+	return <div className={`box`} />;
 }
 
 class Map extends React.Component {
 	constructor(props) {
 		super(props);
 	}
+
+	renderSquare(i) {
+		return <Square className={`box`} key={i} />;
+	}
+
 	render() {
 		let map = [];
-		for (let i = 0; i < this.props.columns; i++) {
-			map.push(<div className="mapRow">{[ ...Array(this.props.rows) ].map(() => Square())}</div>);
+		for (let i = 0; i < this.props.columns + 1; i++) {
+			map.push(
+				<div className="mapRow" key={i}>
+					{[ ...Array(this.props.rows + 1) ].map((el, i) => {
+						return this.renderSquare(i);
+					})}
+				</div>
+			);
 		}
 		return <div className="map"> {map}</div>;
 	}
