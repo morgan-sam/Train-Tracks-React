7455bf216fe6672d13db805da9855f9d16c27a0d
Sam <samuel_morgan_@outlook.com>
2020-01-22 22:35:51 +0000

    renders grid of squares

--- src/index.js
+++ src/index.js
@@ -6,17 +6,24 @@ function Square(props) {
 	return <div className="box" />;
 }
 
+class Map extends React.Component {
+	render() {
+		let row = 5;
+		let column = 7;
+		let map = [];
+		for (let i = 0; i < column; i++) {
+			map.push(<div className="mapRow">{[ ...Array(row) ].map(() => Square())}</div>);
+		}
+		return <div className="map"> {map}</div>;
+	}
+}
+
 class App extends React.Component {
 	render() {
 		return (
 			<div>
 				<h1>Train Tracks</h1>
-				{Square()}
-				{Square()}
-				{Square()}
-				{Square()}
-				{Square()}
-				{Square()}
+				<Map />
 			</div>
 		);
 	}
