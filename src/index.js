141c3cce428f279d05494ce0a8f317b5a5e137be
Sam <samuel_morgan_@outlook.com>
2020-01-22 21:16:43 +0000

    added square function

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function Square(props) {
+	return <div className="box" />;
+}
+
 class App extends React.Component {
 	render() {
 		return (
 			<div>
 				<h1>Train Tracks</h1>
+				{Square()}
+				{Square()}
+				{Square()}
+				{Square()}
+				{Square()}
+				{Square()}
 			</div>
 		);
 	}
