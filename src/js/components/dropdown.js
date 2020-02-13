1200ded5774f295267f716ebd65bdff30c303c43
Sam <samuel_morgan_@outlook.com>
2020-02-13 13:24:01 +0000

    Dropdown list shows on click

--- src/js/components/dropdown.js
+++ src/js/components/dropdown.js
@@ -2,13 +2,25 @@ import React from 'react';
 
 const DROPDOWN_WIDTH = '7rem';
 const DROPDOWN_HEIGHT = '2rem';
+const itemList = [ 'apples', 'berries', 'citrus' ];
 
 class Dropdown extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			defaultValue: 'Dropdown'
+			defaultValue: 'Dropdown',
+			listOpen: false
 		};
+	}
+
+	setListOpenState(boo) {
+		this.setState({
+			listOpen: boo
+		});
+	}
+
+	renderDropDownList() {
+		console.log('123');
 	}
 
 	renderDropDownContainer() {
@@ -18,16 +30,34 @@ class Dropdown extends React.Component {
 			lineHeight: DROPDOWN_HEIGHT,
 			backgroundColor: '#eee',
 			border: '1px black solid',
-			float: 'left',
-			webkitUserSelect: 'none',
-			mozUserSelect: 'none',
+			WebkitUserSelect: 'none',
+			MozUserSelect: 'none',
 			msUserSelect: 'none',
 			userSelect: 'none'
 		};
-		return <div style={containerStyle}>{this.state.defaultValue} ▼</div>;
+		let dropDownList = [];
+		if (this.state.listOpen) {
+			itemList.forEach((el) => {
+				dropDownList.push(
+					<div style={containerStyle} key={el}>
+						{el}
+					</div>
+				);
+			});
+		}
+
+		return (
+			<div>
+				<div style={containerStyle} onClick={() => this.setListOpenState(true)}>
+					{this.state.defaultValue} ▼
+				</div>
+				{dropDownList}
+			</div>
+		);
 	}
 
 	render() {
+		const dropDownList = this.renderDropDownList;
 		return <div>{this.renderDropDownContainer()}</div>;
 	}
 }
