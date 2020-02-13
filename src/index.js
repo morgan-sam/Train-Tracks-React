4c95eeaabb6ecb4bde5a1b7af907e4a84595d9ab
Sam <samuel_morgan_@outlook.com>
2020-02-13 11:25:29 +0000

    Added Meyer's reset.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './js/components/app';
+import './css/reset.css';
 import './css/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
