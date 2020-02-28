501a23bd3d22365d1dad35c40d6255120ff5f21b
Sam <samuel_morgan_@outlook.com>
2020-02-28 11:06:19 +0000

    Background rectangles rotate on hover

--- src/js/components/waveButton.js
+++ src/js/components/waveButton.js
@@ -25,6 +25,43 @@ const WaveButton = ({ className, onClick, text }) => {
 
 	const textStyle = { zIndex: '1' };
 
+	const rectangleStyle = {
+		height: '1000%',
+		width: '300%',
+		position: 'absolute',
+		zIndex: '-1',
+		borderRadius: '50rem',
+		transition: '2s'
+	};
+
+	const startRotation = 0;
+	const endRotation = 180;
+	const rotationOffset = 20;
+	const defaultTransition = 5;
+	const transitionOffset = 0.1;
+
+	const recOneStyle = {
+		backgroundColor: '#D1EEEE',
+		transform: hovered ? `rotate(${startRotation}deg)` : `rotate(${startRotation + endRotation}deg)`,
+		transition: `${defaultTransition}s`
+	};
+
+	const recTwoStyle = {
+		backgroundColor: '#ADEAEA',
+		transform: hovered
+			? `rotate(${startRotation + rotationOffset}deg)`
+			: `rotate(${startRotation + endRotation + rotationOffset}deg)`,
+		transition: `${defaultTransition + transitionOffset}s`
+	};
+
+	const recThreeStyle = {
+		backgroundColor: '#96CDCD',
+		transform: hovered
+			? `rotate(${startRotation + rotationOffset * 2}deg)`
+			: `rotate(${startRotation + endRotation + rotationOffset * 2}deg)`,
+		transition: `${defaultTransition + transitionOffset * 2}s`
+	};
+
 	return (
 		<button
 			className={className}
@@ -33,8 +70,10 @@ const WaveButton = ({ className, onClick, text }) => {
 			onMouseLeave={() => setHoveredState(false)}
 			onClick={() => setTimeout(onClick, 500)}
 		>
+			<div style={{ ...rectangleStyle, ...recOneStyle }} className="rectangle" id="recOne" />
+			<div style={{ ...rectangleStyle, ...recTwoStyle }} className="rectangle" id="recTwo" />
+			<div style={{ ...rectangleStyle, ...recThreeStyle }} className="rectangle" id="recThree" />
 			<div style={textStyle}>{text}</div>
-			<div className="wave" style={waveStyle} />
 		</button>
 	);
 };
