a8db79d033bba6cf69ff5bc9bc028a26a8ab15e1
Sam <samuel_morgan_@outlook.com>
2020-02-28 10:33:37 +0000

    Btn has slider fx

--- src/js/components/waveButton.js
+++ src/js/components/waveButton.js
@@ -3,18 +3,38 @@ import { print } from '../utility/utilityFunctions';
 
 const WaveButton = ({ className, onClick, text }) => {
 	const [ hovered, setHoveredState ] = useState(false);
+
+	const waveStyle = {
+		width: '100%',
+		height: '100%',
+		left: hovered ? '0' : '-100%',
+		position: 'absolute',
+		transition: 'all .35s ease-Out',
+		bottom: '0',
+		backgroundColor: 'salmon',
+		zIndex: '-1'
+	};
+
+	const btnStyle = {
+		position: 'relative',
+		overflow: 'hidden',
+		cursor: 'pointer',
+		background: 'none',
+		zIndex: '0'
+	};
+
+	const textStyle = { zIndex: '1' };
+
 	return (
 		<button
 			className={className}
-			style={{
-				backgroundColor: hovered ? 'red' : 'white',
-				transition: '1s'
-			}}
+			style={btnStyle}
 			onMouseOver={() => setHoveredState(true)}
 			onMouseLeave={() => setHoveredState(false)}
 			onClick={() => setTimeout(onClick, 500)}
 		>
-			{text}
+			<div style={textStyle}>{text}</div>
+			<div className="wave" style={waveStyle} />
 		</button>
 	);
 };
