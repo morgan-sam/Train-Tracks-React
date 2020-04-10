283fb77f8cc405fdd1a6e958211f7eff72ad8b80
Sam <samuel_morgan_@outlook.com>
2020-04-10 17:47:05 +0100

    Created functionAverageSpeedTest Fn

--- src/tests/generateMap.test.js
+++ src/tests/generateMap.test.js
@@ -3,26 +3,22 @@ import { generateNewMap } from '../js/generation/generateMap';
 const size = 8;
 const seed = 986707260499975;
 
-test('Runs generateNewMap function WITHOUT pathFinding', () => {
+const functionAverageSpeedTest = (message, iterations, fn) => {
 	const loggedTimes = [];
-	for (let i = 0; i < 10; i++) {
+	for (let i = 0; i < iterations; i++) {
 		const start = performance.now();
-		generateNewMap({ size, seed, pathFinding: false });
+		fn();
 		const time = performance.now() - start;
 		loggedTimes.push(time);
 	}
 	const average = loggedTimes.reduce((a, b) => a + b) / loggedTimes.length;
-	console.log(`Average time without pathfinding took: \t ${average} ms`);
+	console.log(`${message}: \t ${average} ms`);
+};
+
+test('Runs generateNewMap function WITHOUT pathFinding', () => {
+	generateNewMap({ size, seed, pathFinding: false });
 });
 
 test('Runs generateNewMap function WITH pathFinding', () => {
-	const loggedTimes = [];
-	for (let i = 0; i < 10; i++) {
-		const start = performance.now();
-		generateNewMap({ size, seed, pathFinding: true });
-		const time = performance.now() - start;
-		loggedTimes.push(time);
-	}
-	const average = loggedTimes.reduce((a, b) => a + b) / loggedTimes.length;
-	console.log(`Average time with pathfinding took: \t ${average} ms`);
+	generateNewMap({ size, seed, pathFinding: true });
 });
