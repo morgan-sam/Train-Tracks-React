3888a7971f7316c786c61ebc37da6a0361a7a660
Sam <samuel_morgan_@outlook.com>
2020-04-10 17:53:23 +0100

    Removed functionAverageSpeedTest Fn

--- src/tests/generateMap.test.js
+++ src/tests/generateMap.test.js
@@ -1,19 +1,7 @@
 import { generateNewMap } from '../js/generation/generateMap';
 
-const size = 8;
+const size = 10;
 const seed = 986707260499975;
-
-const functionAverageSpeedTest = (message, iterations, fn) => {
-	const loggedTimes = [];
-	for (let i = 0; i < iterations; i++) {
-		const start = performance.now();
-		fn();
-		const time = performance.now() - start;
-		loggedTimes.push(time);
-	}
-	const average = loggedTimes.reduce((a, b) => a + b) / loggedTimes.length;
-	console.log(`${message}: \t ${average} ms`);
-};
 
 test('Runs generateNewMap function WITHOUT pathFinding', () => {
 	generateNewMap({ size, seed, pathFinding: false });
