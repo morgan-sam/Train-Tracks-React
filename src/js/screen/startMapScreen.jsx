c534d68ef232c62bbcecb5c3e608a73b619294bf
Sam <samuel_morgan_@outlook.com>
2020-04-13 19:52:37 +0100

    startMapOptionsContainer styling

--- src/js/screen/startMapScreen.jsx
+++ src/js/screen/startMapScreen.jsx
@@ -24,64 +24,66 @@ export const StartMapScreen = (props) => {
 
 	return (
 		<div className="menu startMapScreen">
-			<p className="startMapOptionLabel" key={'Map Size Label'}>
-				Map Size
-			</p>
-			<Dropdown
-				key={'selectMapSize'}
-				style={{ width: '3rem', height: '2rem', textAlign: 'center', padding: '0 1.25rem 0 0.5rem' }}
-				options={getMapSizeOptions()}
-				className="mapSizeOption"
-				placeholder={`${props.gameState.size}x${props.gameState.size}`}
-				onHover={() => null}
-				onChange={(item) => {
-					props.setGameState({ ...props.gameState, size: item.value });
-				}}
-			/>
-			<p className="startMapOptionLabel" key={'Map Seed Label'}>
-				Map Seed
-			</p>
-			<div className="mapSeedOptionRow">
-				<input
-					key="mapSeedInput"
-					type="text"
-					className="mapSeedInput"
-					id="mapSeedInput"
-					defaultValue={props.gameState.seed}
-					onChange={(e) => props.setGameState({ ...props.gameState, seed: e.target.value })}
-					style={{ width: '8rem', textAlign: 'center' }}
-				/>
-				<div
-					key="rerollSeedBtn"
-					className="rerollSeedBtn"
-					onClick={(e) => {
-						e.preventDefault();
-						props.setGameState({ ...props.gameState, seed: getRandomSeed() });
+			<div className="startMapOptionsContainer">
+				<p className="startMapOptionLabel" key={'Map Size Label'}>
+					Map Size
+				</p>
+				<Dropdown
+					key={'selectMapSize'}
+					style={{ width: '3rem', height: '2rem', textAlign: 'center', padding: '0 1.25rem 0 0.5rem' }}
+					options={getMapSizeOptions()}
+					className="mapSizeOption"
+					placeholder={`${props.gameState.size}x${props.gameState.size}`}
+					onHover={() => null}
+					onChange={(item) => {
+						props.setGameState({ ...props.gameState, size: item.value });
 					}}
-				>
-					<span className="diceSymbol">⚄</span>
-				</div>
-			</div>
-			<div className="startOptionOptionRow">
-				<input
-					type="checkbox"
-					className="startOptionCheckbox"
-					onChange={(e) => props.setGameState({ ...props.gameState, balloonCloud: !e.target.checked })}
 				/>
-				<p className="startOptionLabel">Disable Win Screen Balloon Cloud</p>
-				<div className="startOptionQuestionBox">?</div>
-				<div className="startOptionExplanation">
-					Disables the balloon cloud effect at the end of a game win. Can increase performance on old
-					browsers/PCs.
+				<p className="startMapOptionLabel" key={'Map Seed Label'}>
+					Map Seed
+				</p>
+				<div className="mapSeedOptionRow">
+					<input
+						key="mapSeedInput"
+						type="text"
+						className="mapSeedInput"
+						id="mapSeedInput"
+						defaultValue={props.gameState.seed}
+						onChange={(e) => props.setGameState({ ...props.gameState, seed: e.target.value })}
+						style={{ width: '8rem', textAlign: 'center' }}
+					/>
+					<div
+						key="rerollSeedBtn"
+						className="rerollSeedBtn"
+						onClick={(e) => {
+							e.preventDefault();
+							props.setGameState({ ...props.gameState, seed: getRandomSeed() });
+						}}
+					>
+						<span className="diceSymbol">⚄</span>
+					</div>
 				</div>
+				<div className="startOptionOptionRow">
+					<input
+						type="checkbox"
+						className="startOptionCheckbox"
+						onChange={(e) => props.setGameState({ ...props.gameState, balloonCloud: !e.target.checked })}
+					/>
+					<p className="startOptionLabel">Disable Win Screen Balloon Cloud</p>
+					<div className="startOptionQuestionBox">?</div>
+					<div className="startOptionExplanation">
+						Disables the balloon cloud effect at the end of a game win. Can increase performance on old
+						browsers/PCs.
+					</div>
+				</div>
+				<WaveButton
+					key={'generateMapBtn'}
+					onClick={() => {
+						props.generateMap();
+					}}
+					text={'Generate Map'}
+				/>
 			</div>
-			<WaveButton
-				key={'generateMapBtn'}
-				onClick={() => {
-					props.generateMap();
-				}}
-				text={'Generate Map'}
-			/>
 			<ReturnToMainMenuBtn {...props} />
 		</div>
 	);
