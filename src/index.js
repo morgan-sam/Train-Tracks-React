0ba6a5cfa7fc7f39bcad95dcd01bd9024671e59a
Sam <samuel_morgan_@outlook.com>
2020-04-13 17:46:04 +0100

    balloon css sep file

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './js/components/App';
 import './css/reset.css';
 import './css/index.css';
+import './css/balloon.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
