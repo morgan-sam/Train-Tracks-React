9c9cdfe2e9d5d4fc6867cf5b5e3fbfeb4c8cc6c6
Sam <samuel_morgan_@outlook.com>
2020-04-13 19:13:28 +0100

    Changed game css import location, fixed font

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './js/components/App';
 import './css/reset.css';
 import './css/index.css';
-import './css/game.css';
 import './css/balloon.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/js/components/Game.jsx
+++ src/js/components/Game.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import '../../css/game.css';
+
 import Map from './Map';
 import OptionsButtons from './OptionsButtons';
 import SaveMapDisplay from './SaveMapDisplay';
