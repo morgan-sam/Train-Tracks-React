07fa4816808656a95aa836acede05448c5527157
Sam <samuel_morgan_@outlook.com>
2020-04-15 16:52:35 +0100

    Fixed cross sizing

--- src/js/generation/icon/generateCrossTrackIcon.js
+++ src/js/generation/icon/generateCrossTrackIcon.js
@@ -28,15 +28,16 @@ const clearCross = (canvas) => {
 	let context = canvas.getContext('2d');
 	let x = canvas.width / 2;
 	let y = canvas.height / 2;
-	const cw = 6; //cross width
-	const cl = 40; //cross length
+	const cw = 5; //cross width
+	const cl = 50; //cross length
+
 	context.save();
 	context.beginPath();
 	context.translate(x, y);
 	context.rotate(45 * Math.PI / 180);
-	context.rect(-2, -cl / 2, cw, cl);
+	context.rect(-cw / 2, -cl / 2, cw, cl);
 	context.rotate(90 * Math.PI / 180);
-	context.rect(-2, -cl / 2, cw, cl);
+	context.rect(-cw / 2, -cl / 2, cw, cl);
 	context.clip();
 	context.translate(-x, -y);
 	context.clearRect(0, 0, canvas.width, canvas.height);
